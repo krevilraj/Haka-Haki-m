6e107aa0f17919ae00d86fbb36e0a75f0f5e3970
eyenish <wagleanish999@gmail.com>
2018-07-03 17:57:10 +0545

    tuesday anish

--- app/src/main/java/com/mayurit/hakahaki/Fragments/FragmentHome.java
+++ app/src/main/java/com/mayurit/hakahaki/Fragments/FragmentHome.java
@@ -1,17 +1,26 @@
 package com.mayurit.hakahaki.Fragments;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.mayurit.hakahaki.Helpers.Constant;
+import com.mayurit.hakahaki.Helpers.DatabaseHelper;
 import com.mayurit.hakahaki.Helpers.RetrofitAPI;
 import com.mayurit.hakahaki.Model.NewsListModel;
 import com.mayurit.hakahaki.R;
@@ -32,10 +41,15 @@ public class FragmentHome extends Fragment {
     private String mParam2;
     Context context;
     int category_id;
+    CardView nefej;
     TextView mainNews1_title,mainNews2_title,mainNews3_title,mainNews4_title,
             mainNews1_content,mainNews2_content,mainNews3_content,mainNews4_content;
     ImageView mainNews1_image,mainNews2_image,mainNews3_image,mainNews4_image;
     private Context ctx;
+    private LinearLayout lnrlayoutNews;
+    DatabaseHelper databaseHelper;
+    RelativeLayout rel_container;
+    View view ;
     public FragmentHome() {
         // Required empty public constructor
     }
@@ -64,8 +78,9 @@ public class FragmentHome extends Fragment {
                              Bundle savedInstanceState) {
 
         category_id =34;
-        View view = inflater.inflate(R.layout.fragment_home,null);
+       view = inflater.inflate(R.layout.fragment_home,null);
         // Inflate the layout for this fragment
+        databaseHelper = new DatabaseHelper(context);
         mainNews1_title = view.findViewById(R.id.mainNews1_title);
         mainNews2_title = view.findViewById(R.id.mainNews2_title);
         mainNews3_title = view.findViewById(R.id.mainNews3_title);
@@ -81,8 +96,9 @@ public class FragmentHome extends Fragment {
         mainNews3_image = view.findViewById(R.id.mainNews3_image);
         mainNews4_image = view.findViewById(R.id.mainNews4_image);
 
-        fetchNews();
+        lnrlayoutNews = view.findViewById(R.id.lnrlayoutNews);
 
+        mobile_data();
 
         return view;
 
@@ -101,24 +117,20 @@ public class FragmentHome extends Fragment {
             @Override
             public void onResponse(Call<List<NewsListModel>> call, Response<List<NewsListModel>> response) {
                 list.addAll(response.body());
+                Log.i("list","data = " +list.get(0).getPostTitle());
 
-                    Log.i("list","data = " +list.get(0).getPostTitle());
+                display(list);
 
+                lnrlayoutNews.setVisibility(View.VISIBLE);
+        for(NewsListModel info:list){
+            ContentValues cv = new ContentValues();
+            cv.put("post_title", info.getPostTitle());
+            cv.put("category_id", 34);
+            cv.put("post_date", info.getPostDate());
+            cv.put("post_description", info.getPostExcerpt());
+            databaseHelper.insertMainNews(cv);
+        }
 
-               mainNews1_title.setText(list.get(0).getPostTitle());
-               mainNews2_title.setText(list.get(1).getPostTitle());
-               mainNews3_title.setText(list.get(2).getPostTitle());
-               mainNews4_title.setText(list.get(3).getPostTitle());
-
-               mainNews1_content.setText(list.get(0).getPostExcerpt());
-                mainNews2_content.setText(list.get(1).getPostExcerpt());
-                mainNews3_content.setText(list.get(2).getPostExcerpt());
-                mainNews4_content.setText(list.get(3).getPostExcerpt());
-
-                Glide.with(context).load(list.get(0).getImageId()).into(mainNews1_image);
-                Glide.with(context).load(list.get(1).getImageId()).into(mainNews2_image);
-                Glide.with(context).load(list.get(2).getImageId()).into(mainNews3_image);
-                Glide.with(context).load(list.get(3).getImageId()).into(mainNews4_image);
             }
 
             @Override
@@ -126,5 +138,69 @@ public class FragmentHome extends Fragment {
            //     Toast.makeText(FragmentHome.this, "Failed to load", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+    public void mobile_data(){
+       Log.i("msz", "v="+databaseHelper.countRowTable("tbl_news"));
+            if((databaseHelper.countRowTable("tbl_news") == 0)){
+                netCheck();
+            }
+            else {
+                List<NewsListModel> listModel = databaseHelper.getQAList("34");
+               // list.addAll(databaseHelper.getQAList("34"));
+                Log.i("msz","size = "+list.size());
+                display(listModel);
+
+
+        /*        List<NewsListModel> listModel1 = databaseHelper.getQAList("28");
+               // list.addAll(databaseHelper.getQAList("34"));
+                Log.i("msz","size = "+list.size());
+                display(listModel1);
+                */
+                lnrlayoutNews.setVisibility(View.VISIBLE);
+            }
+
+
+    }
+
+    public void netCheck() {
+
+        ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (activeNetwork != null) { // connected to the internet
+            fetchNews();
+        } else {
+            rel_container = (RelativeLayout) view.findViewById(R.id.rel_container);
+            Snackbar snackbar = Snackbar.make(rel_container, "No internet connection!", Snackbar.LENGTH_INDEFINITE).setAction("RETRY", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    netCheck();
+                }
+            });
+            snackbar.setActionTextColor(Color.RED);
+
+            View sbView = snackbar.getView();
+            TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+            textView.setTextColor(Color.YELLOW);
+            snackbar.show();
+        }
+
+    }
+
+    private void display(List<NewsListModel> list) {
+
+        mainNews1_title.setText(list.get(0).getPostTitle());
+        mainNews2_title.setText(list.get(1).getPostTitle());
+        mainNews3_title.setText(list.get(2).getPostTitle());
+        mainNews4_title.setText(list.get(3).getPostTitle());
+
+        mainNews1_content.setText(list.get(0).getPostExcerpt());
+        mainNews2_content.setText(list.get(1).getPostExcerpt());
+        mainNews3_content.setText(list.get(2).getPostExcerpt());
+        mainNews4_content.setText(list.get(3).getPostExcerpt());
+
+        Glide.with(context).load(list.get(0).getImageId()).into(mainNews1_image);
+        Glide.with(context).load(list.get(1).getImageId()).into(mainNews2_image);
+        Glide.with(context).load(list.get(2).getImageId()).into(mainNews3_image);
+        Glide.with(context).load(list.get(3).getImageId()).into(mainNews4_image);
     }
 }
